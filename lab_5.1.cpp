9c45e68db7f89f90c6713c6bed14f841df2b304c
vittkoo <dmytrovych.vv@gmail.com>
2024-10-01 20:47:17 +0300

    commit_1

---
+++ lab_5.1.cpp
@@ -0,0 +1,22 @@
+//Lab 5.1
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double f(const double a, const double b, const double c);
+
+int main()
+{
+	double s, t;
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+
+	double v = (f(1, pow(t, 2), s) + f(t, pow(s, 2), 1)) / 1+pow(f(1, t*s, 1), 2);
+	cout << "v = " << v << endl;
+
+	return 0;
+}
+double f(const double a, const double b, const double c)
+{
+	return (pow(sin(a * b * c), 2)) / (a * a + b * b + c * c);
+}
